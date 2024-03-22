3567284d0f3f68b2600be4f40d7ed26ab333bc85
atila <atiorh@icloud.com>
2024-03-21 19:37:43 -0700

    Optionally disable token timestamps for models without alignment_heads

--- scripts/generate_model.py
+++ scripts/generate_model.py
@@ -13,6 +13,7 @@ from pprint import pprint
 
 from argmaxtools import _sdpa
 from argmaxtools.utils import get_logger
+from argmaxtools import test_utils
 from huggingface_hub import HfApi, hf_hub_download
 
 from tests import test_audio_encoder, test_text_decoder
@@ -20,6 +21,7 @@ from whisperkit._constants import COMPRESSION_REPO_ID, MODEL_REPO_ID
 
 logger = get_logger(__name__)
 
+test_utils.TEST_MIN_SPEEDUP_VS_CPU = 0.3
 
 def cli():
     f""" Generates Whisper models and publishes them to hf.co/{MODEL_REPO_ID} """
@@ -92,6 +94,14 @@ def cli():
         args.persistent_cache_dir += f"_{args.repo_path_suffix}"
 
     logger.info(f"Generating {args.model_version} files")
+
+    # FIXME(atiorh): Remove this once distil-whisper-* models are updated
+    args.disable_token_timestamps = False
+    if "distil-whisper" in args.model_version:
+        logger.info(
+            "Disabling token-level timestamps due to missing alignment_heads in distil-whisper-* models"
+        )
+        args.disable_token_timestamps = True
 
     # Generate WhisperTextDecoder
     args.test_seq_len = args.text_decoder_max_sequence_length
